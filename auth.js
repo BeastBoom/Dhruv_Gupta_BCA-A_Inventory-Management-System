0fa348f3487ef0aa6aea6348d2191e12ba9e13e0
Dhruv Gupta <97024503+BeastBoom@users.noreply.github.com>
2025-02-13 15:28:11 +0530

    removed uneccesary spacing

--- auth.js
+++ auth.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
-        
       })
         .then((res) => res.json())
         .then((data) => {
